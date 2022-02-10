832eebfd77b3ff9d03be7485bdc29cd1cc9e683d
esteban munoz <esteban081313@gmail.com>
2022-02-10 10:37:47 -0500

    Commit 2

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,6 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Sebastián Cataño Gil
+// 000160532
+// sebsatian.catano@upb.edu.co
 (START)
     @START
     0;JMP
