7990145f7015b61bbea548a17d6d9cd8e66c7564
Sebastián Cataño Gil <sebastian.catano@upb.edu.co>
2022-02-12 13:15:54 -0500

    Commit 8

--- problem/program.asm
+++ problem/program.asm
@@ -2,13 +2,25 @@
 // 000160532
 // sebsatian.catano@upb.edu.co
 (START)
-@24576
-D=M
+@16384
+D=A
 @0
 M=D
-@24576
+//Linea 4 @24576
 D=M
 @70
 D=A-D
 @FILLCOLOR
 D;JEQ
+@24576
+D=M
+@67
+D=A-D
+@CLEAR
+D;JEQ
+@4
+0;JMP
+//Linea FILLCOLOR
+
+
+
